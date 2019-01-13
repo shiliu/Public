8f0eb458657bea88f6fecbf9c4f5f496d8cd573c
SHI LIU <shiliu@ddrr.org>
2019-01-13 19:07:32 +0800

    Save

--- choco/install.ps1
+++ choco/install.ps1
@@ -1,9 +1,38 @@
-Write-Host "Hello world!"
+$isInMsCorp = Test-Path "\\stcsuz"
 
-# Install the Choco if not yet
-if (!(Test-Path "$ENV:ChocolateyInstall1choco.exe"))
-{
-    # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#
+# Step 1: Install the Choco if not yet
+#
+if (!(Test-Path "$ENV:ChocolateyInstall\bin\choco.exe")){
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    $ENV:Path = "$ENV:ALLUSERSPROFILE\chocolatey\bin;$ENV:Path"
 }
 
-Write-Host "Packages: $Packages"
+#
+# Step 2: Update sources
+#
+
+# liushi@microsoft
+if ($isInMsCorp){
+    if (Test-Path "\\stcsuz\root\users\liushi\choco\packages"){
+        choco source add -n=liushi -s="\\stcsuz\root\users\liushi\choco\packages" --priority=20
+    }
+}
+
+# ddrr
+choco source add -n=ddrr -s="http://nuget.ddrr.org/api/odata" --priority=10
+
+
+#
+# Step 3: Install packages
+#
+
+# Install the packages defined in default config file in MS corp.
+if ($isInMsCorp){
+    if (Test-Path "\\stcsuz\root\users\liushi\choco\default.config"){
+        choco install "\\stcsuz\root\users\liushi\choco\default.config" -y
+    }
+}
+
+# Install my very basic packages
+choco install ddrr.SetupEnv -y
