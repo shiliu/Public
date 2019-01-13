fad63a61446dcbeacea3e62be823d1307a16a359
SHI LIU <shiliu@ddrr.org>
2019-01-13 16:34:00 +0800

    Update install.ps1

--- choco/install.ps1
+++ choco/install.ps1
@@ -1,1 +1,13 @@
+param(
+    [string] $Packages
+)
+
 Write-Host "Hello world!"
+
+# Install the Choco if not yet
+if (!(Test-Path "$ENV:ChocolateyInstall1choco.exe"))
+{
+    # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host "Packages: $Packages"
